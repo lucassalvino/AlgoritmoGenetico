74080c45bf4755278502a69d8d55042da676ff64
Lucas <Salvino>
2020-01-21 18:55:40 -0300

    Add project files.

---
+++ AlgoritmoGenetico/GeneticAlgorithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneticAlgorithm
+{
+    public class GeneticAlgorithm<GeneType>
+    {
+        public List<Generation<GeneType>> Population { get; set; }
+        public void CreateInitialPopulation(int numberOfIndividuals, int numberOfGenesOfIndividuals)
+        {
+
+        }
+    }
+}
---
+++ TesteConsoleAlgoritmoGenetico/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TesteConsoleAlgoritmoGenetico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
